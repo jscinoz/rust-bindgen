a81afc5812db691aa37fae7a006586ea64c9d2d8
Yamakaky <yamakaky@yamaworld.fr>
2016-03-15 20:25:28 +0100

    [gen.rs] Remove `tag_dup_decl`.
    Seems unneeded, the tests passes without it.

--- src/gen.rs
+++ src/gen.rs
@@ -212,12 +212,11 @@ pub fn gen_mod(
             span: None
         }
     });
-    let uniq_globs = tag_dup_decl(&globs);
 
     let mut fs = vec!();
     let mut vs = vec!();
     let mut gs = vec!();
-    for g in uniq_globs.into_iter() {
+    for g in globs.into_iter() {
         match g {
             GOther => {}
             GFunc(_) => fs.push(g),
@@ -324,57 +323,6 @@ fn mk_extern(ctx: &mut GenCtx, links: &[(String, LinkType)],
         vis: ast::Visibility::Inherited,
         span: ctx.span
     })
-}
-
-fn tag_dup_decl(gs: &[Global]) -> Vec<Global> {
-    fn check(name1: &str, name2: &str) -> bool {
-        !name1.is_empty() && name1 == name2
-    }
-
-    fn is_dup(g1: &Global, g2: &Global) -> bool {
-        match (g1, g2) {
-          (&GType(ref ti1), &GType(ref ti2)) => {
-              let a = ti1.borrow();
-              let b = ti2.borrow();
-              check(&a.name[..], &b.name[..])
-          },
-          (&GComp(ref ci1), &GComp(ref ci2)) |
-          (&GCompDecl(ref ci1), &GCompDecl(ref ci2)) => {
-              let a = ci1.borrow();
-              let b = ci2.borrow();
-              check(&a.name[..], &b.name[..])
-          },
-          (&GEnum(ref ei1), &GEnum(ref ei2)) |
-          (&GEnumDecl(ref ei1), &GEnumDecl(ref ei2)) => {
-              let a = ei1.borrow();
-              let b = ei2.borrow();
-              check(&a.name[..], &b.name[..])
-          },
-          (&GVar(ref vi1), &GVar(ref vi2)) |
-          (&GFunc(ref vi1), &GFunc(ref vi2)) => {
-              let a = vi1.borrow();
-              let b = vi2.borrow();
-              check(&a.name[..], &b.name[..])
-          },
-          _ => false
-        }
-    }
-
-    if gs.is_empty() {
-        return vec!();
-    }
-
-    let mut res: Vec<Global> = vec!();
-    res.push(gs[0].clone());
-
-    for (i, gsi) in gs.iter().enumerate().skip(1) {
-        let dup = gs.iter().take(i).any(|item| is_dup(&item, &gsi));
-        if !dup {
-            res.push(gsi.clone());
-        }
-    }
-
-    res
 }
 
 /// Converts a C typedef to Rust AST Items.
